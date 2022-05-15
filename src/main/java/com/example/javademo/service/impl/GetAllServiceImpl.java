52e6261a291e22d6697840f7e20d25433d447841
ahmed shalash <ashalash38@gmail.com>
2022-05-16 01:41:00 +0200

    for testing purposes

--- src/main/java/com/example/javademo/service/impl/GetAllServiceImpl.java
+++ src/main/java/com/example/javademo/service/impl/GetAllServiceImpl.java
@@ -5,7 +5,10 @@ import com.example.javademo.service.GetAll;
 public class GetAllServiceImpl implements GetAll {
     @Override
     public void getAll() {
+
             System.out.println("java test");
-        System.out.println("java test");
+
+            System.out.println("java test");
+
     }
 }
