638ba4e92690275366d871fecfd7aaf43e4644a5
ahmed shalash <ashalash38@gmail.com>
2022-05-25 18:42:27 +0200

    add jenkines file

--- src/test/java/com/example/javademo/JavaDemoApplicationTests.java
+++ src/test/java/com/example/javademo/JavaDemoApplicationTests.java
@@ -2,12 +2,16 @@ package com.example.javademo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class JavaDemoApplicationTests {
 
     @Test
     void contextLoads() {
+        assertNotNull("shalash");
     }
 
 }
